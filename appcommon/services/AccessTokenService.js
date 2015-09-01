47007b6c68e2ebab71e76e467186dc558c640a56
LocNT <locnt@vn.ibm.com>
2015-09-01 09:52:44 +0700

    request friend

--- appcommon/services/AccessTokenService.js
+++ appcommon/services/AccessTokenService.js
@@ -9,7 +9,7 @@ var message = require("../message/en");
 var checkValidateUtil = require("../utils/CheckValidateUtil");
 
 var checkAccessToken = function(req, res, next){
-    var accessToken = req.body.accessToken;
+    var accessToken = req.body.accessToken || req.query.accessToken;
     var responseObj = new ResponseServerDto();
 
     if(checkValidateUtil.isEmptyFeild(accessToken)){
